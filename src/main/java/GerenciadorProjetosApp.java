b51e31bdcd1c7d6be0eaa369371c8c9fd5a5537b
aruwer <amandamaffini@gmail.com>
2025-05-30 21:32:19 -0300

    feat: lista de usuario

--- src/main/java/GerenciadorProjetosApp.java
+++ src/main/java/GerenciadorProjetosApp.java
@@ -1,9 +1,13 @@
+import models.Usuario;
+import views.user.UsuarioList;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GerenciadorProjetosApp extends JFrame {
     private  CardLayout cardLayout;
     private JPanel mainPanel;
+    private final String LIST_USER = "LIST_USER";
     private final String EMPTY_SCREEN = "EMPTY_SCREEN";
     public GerenciadorProjetosApp(){
         setTitle("Gerenciador de Projeto");
@@ -19,20 +23,39 @@ public class GerenciadorProjetosApp extends JFrame {
                         SwingConstants.CENTER),
                 BorderLayout.CENTER);
 
+        UsuarioList usuariosList = new UsuarioList();
+
         this.mainPanel.add(emptyPanel, EMPTY_SCREEN);
+        this.mainPanel.add(usuariosList, LIST_USER);
+
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("Menu");
 
         JMenuItem listUserItem = new JMenuItem("Listar Usuário");
+        JMenuItem listTaskItem = new JMenuItem("Listar Tarefas");
         JMenuItem exitItem = new JMenuItem("Sair");
 
         menu.add(listUserItem);
+        menu.add(listTaskItem);
         menu.add(exitItem);
 
         menuBar.add(menu);
         setJMenuBar(menuBar);
 
         add(mainPanel);
+
+        listUserItem.addActionListener(event -> {
+            this.cardLayout.show(mainPanel, LIST_USER);
+        });
+
+        listTaskItem.addActionListener(event -> {
+            JOptionPane.showMessageDialog(this,
+                    "Lista de Projeto só se eu repetir a UC!");
+        });
+
+        exitItem.addActionListener(event -> {
+            dispose();
+        });
     }
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
